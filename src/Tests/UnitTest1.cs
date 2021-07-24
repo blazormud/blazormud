d045e86fb6b45e15b7e2f384d6b5444ff5ed9338
Marc King <marc.king@gmail.com>
2021-07-24 11:37:41 -0400

    Add unit test project

---
+++ src/Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BlazorMUD.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
